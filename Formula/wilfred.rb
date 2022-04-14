43d70736594eaf7ce500943fa551f4d869f6b11c
Vilhelm Prytz <vilhelm@prytznet.se>
2022-04-14 09:04:15 +0200

    try alternative method of replacing development and YYYY-MM-DD

--- Formula/wilfred.rb
+++ Formula/wilfred.rb
@@ -101,8 +101,8 @@ class Wilfred < Formula
     end
 
     def install
-      system 'sed -i "s/development/`git log -1 --format="%H" || echo error`/g" wilfred/version.py'
-      system 'sed -i "s/YYYY-MM-DD/`(git log -1 --format="%at" || echo error) | (xargs -I{} date -d @{} +%Y-%m-%d || echo error)`/g" wilfred/version.py'
+      system 'python -c "print(open(\'wilfred/version.py\').read().replace(\'development\',\'`git log -1 --format="%H" || echo error`\'))"'
+      system 'python -c "print(open(\'wilfred/version.py\').read().replace(\'YYYY-MM-DD\',\'`(git log -1 --format="%at" || echo error) | (xargs -I{} date -d @{} +%Y-%m-%d || echo error)`\'))"'
       virtualenv_install_with_resources
     end
 
